75e7ffd2133faa06c72013a36781d5b6ad35290d
Maxim <madear0000@bk.ru>
2024-08-14 09:57:56 +0300

    some change logic js

---
+++ src/components/carousel-buttons/sections/carousel-rooms.js
@@ -0,0 +1,81 @@
+export default function carouselRooms() {
+    const slider = document.querySelector('.rooms__area__carousel');
+    const buttonForRight = document.getElementById('right-slider-rooms');
+    const buttonForLeft = document.getElementById('left-slider-rooms');
+    const labelsForSliderRadioButton = document.querySelectorAll('.slider-for-rooms');
+    let offset = 0;
+    let count = 0;
+    
+    
+    labelsForSliderRadioButton[0].style.background = "#FAC663";
+    
+    function checkForRadioButtonPlus() {
+        count++;
+        for (let i = 0; i < 3; i++) {
+            if (i == count) {
+                labelsForSliderRadioButton[i].style.background = "#FAC663";
+            } else {
+                labelsForSliderRadioButton[i].style.background = "#E1E1E1";
+            }
+        }
+    }
+    
+    function checkForRadioButtonMinus() {
+        count--;
+        for (let i = 0; i < 3; i++) {
+            if (i == count) {
+                labelsForSliderRadioButton[i].style.background = "#FAC663";
+            } else {
+                labelsForSliderRadioButton[i].style.background = "#E1E1E1"
+            }
+        }
+    }
+    
+    
+    
+    buttonForRight.addEventListener('click', () => {
+        if (window.innerWidth > 768) {
+            if (offset > -1325) {
+                offset -= 662.5;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonPlus();
+            }
+        } else if (window.innerWidth > 320 && window.innerWidth <= 768) {
+            if (offset > -1325) {
+                offset -= 662.5;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonPlus();
+            }
+        } else if (window.innerWidth <= 320) {
+            if (offset > -1286) {
+                offset -= 643;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonPlus();
+            }
+        } 
+    });
+    
+    buttonForLeft.addEventListener('click', () => {
+        if (window.innerWidth > 768) {
+            if (offset < 0) {
+                offset  += 662.5;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonMinus();
+            }  
+        }  else if (window.innerWidth > 320 && window.innerWidth <= 768) {
+            if (offset < 0) {
+                offset  += 662.5;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonMinus();
+            }  
+        } else if (window.innerWidth <= 320) {
+            if (offset < 0) {
+                offset  += 643;
+                slider.style.left = offset + 'rem';
+                checkForRadioButtonMinus();
+            }  
+        }
+    
+    })
+}
+
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import '../../components/container/container';
 import '../../components/banner/banner';
 import '../../components/advantages/advantages';
 import '../../components/rooms/rooms';
-import '../../components/carousel-buttons/carousel-buttons';
 import '../../components/reviews/reviews';
 import '../../components/map/map';
 import '../../components/footer/footer';
+import '../../components/carousel-buttons/carousel-buttons';
